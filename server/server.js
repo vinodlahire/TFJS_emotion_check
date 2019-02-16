737ff17f8bd0fdf51f4719bc6bd4f4b146d8f301
vinodlahire <vinodlahire@gmail.com>
2019-02-16 20:20:19 +0000

    model files added

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.use(function(req,res,next){
+	console.log(`${new Date()} - ${req.method} request for ${req.url}`);
+	next();
+});
+
+app.use(express.static("../static"));
+
+app.listen(3000,function(){
+	console.log("serving you on port3000")
+});
---
+++ static/.ipynb_checkpoints/imagenet_classes-checkpoint.js
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2017 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+const IMAGENET_CLASSES = {
+  0: 'angry',
+  1: 'disgust',
+  2: 'fear',
+  3: 'happy',
+  4: 'neutral',
+  5: 'sad',
+  6: 'surprise',
+};
